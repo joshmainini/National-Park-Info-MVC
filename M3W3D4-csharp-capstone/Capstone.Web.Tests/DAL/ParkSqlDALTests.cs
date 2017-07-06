3f54210c007db662f4dabf3b8bf2d503550fc1e4
Josh Mainini <joshmainini@outlook.com>
2017-07-06 14:51:19 -0400

    working on tests

---
+++ M3W3D4-csharp-capstone/Capstone.Web.Tests/DAL/ParkSqlDALTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Web.Models;
+using Capstone.Web.DAL;
+
+namespace Capstone.Web.Tests.DAL
+{
+	[TestClass]
+	public class ParkSqlDALTests
+	{
+		private TransactionScope tran;
+		private string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=NationalParkGeek;Integrated Security=True";
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			tran = new TransactionScope();
+		}
+
+		
+		[TestCleanup]
+		public void Cleanup()
+		{
+			tran.Dispose();
+		}
+
+		[TestMethod]
+		public void GetAllParksTest()
+		{
+			List<Park> result = new List<Park>();
+
+			ParkSqlDAL parks = new ParkSqlDAL(connectionString);
+
+			result = parks.GetAllParks();
+
+			Assert.AreEqual(10, result.Count);
+		}
+		[TestMethod]
+		public void GetParkTest()
+		{
+			Park result = new Park();
+
+			ParkSqlDAL parks = new ParkSqlDAL(connectionString);
+
+			result = parks.GetPark("ENP");
+
+			Assert.AreEqual(1, result.Count);
+		}
+		[TestMethod]
+		public void GetParkTest_name()
+		{
+			Park result = new Park();
+
+			ParkSqlDAL parks = new ParkSqlDAL(connectionString);
+
+			result = parks.GetPark("ENP");
+
+			Assert.AreEqual("Everglades National Park", result.Name);
+		}
+	}
+}
---
+++ M3W3D4-csharp-capstone/Capstone.Web.Tests/DAL/WeatherSqlDALTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone.Web.Tests.DAL
+{
+	[TestClass]
+	public class WeatherSqlDALTest
+	{
+		private TransactionScope tran;
+		private string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=NationalParkGeek;Integrated Security=True";
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			tran = new TransactionScope();
+		}
+
+
+		[TestCleanup]
+		public void Cleanup()
+		{
+			tran.Dispose();
+		}
+
+		[TestMethod]
+		public void GetWeatherTest_ParkCode()
+		{
+			Weather result = new Weather();
+
+			WeatherSqlDAL weather = new WeatherSqlDAL(connectionString);
+
+			result = weather.GetWeather("ENP");
+
+			Assert.AreEqual("ENP", result.ParkCode);
+		}
+	}
+}
